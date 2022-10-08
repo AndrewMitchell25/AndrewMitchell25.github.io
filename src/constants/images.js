9207128963fe2475846972249819b0db38297a97
AndrewMitchell25 <andrew.j.mitchell.247@gmail.com>
2022-10-08 17:04:32 -0400

    Navbar, Header, and About

---
+++ src/constants/images.js
@@ -0,0 +1,20 @@
+import ctf from "../assets/capture-the-flag.png"
+import logo from "../assets/logo.png"
+import profile from "../assets/profile.png"
+import circle from "../assets/circle.svg"
+import javascript from "../assets/javascript.png"
+import python from "../assets/python.png"
+import react from "../assets/react.png"
+
+
+const images = {
+    logo,
+    ctf,
+    profile,
+    circle,
+    javascript,
+    python,
+    react
+}
+
+export default images;
--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./About.scss"
+import { motion } from 'framer-motion'
+import { images } from '../../constants'
+
+const abouts = [
+  { title: 'Web Development', description: "I am a good web developer", imgUrl: ""},
+  { title: 'Web Design', description: "I am a good web designer", imgUrl: ""},
+  { title: 'UI/UX', description: "I am a good ui/ux developer", imgUrl: ""}
+]
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+      <h2 className='head-text'>I Know That <span>Good Development</span><br /> means <span>Good Business</span></h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{opacity: 1}}
+            whileHover={{scale:1.1}}
+            transition={{duration:0.5, type: 'tween'}}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title}/>
+            <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
+            <h2 className='p-text' style={{marginTop: 10}}>{about.description}</h2>
+          </motion.div>
+        ))}
+      </div>
+    </>
   )
 }
